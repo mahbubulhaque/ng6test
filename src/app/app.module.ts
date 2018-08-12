f16351cef57690513fabef1bbb84ba419e8c4a7f
mahbubul <rabbi.sus@gmail.com>
2018-08-12 09:13:31 +0600

    ng2-file-upload added and used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,25 @@ import { PostsComponent } from './posts/posts.component';
 import { UsersComponent } from './users/users.component';
 import { DetailsComponent } from './details/details.component';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    PostsComponent,
-    UsersComponent,
-    DetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        PostsComponent,
+        UsersComponent,
+        DetailsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FileUploadModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 import { DataService } from '../data.service';
 import { Observable } from 'rxjs';
+
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'app-users',
@@ -10,13 +13,24 @@ import { Observable } from 'rxjs';
 export class UsersComponent implements OnInit {
 
 	users$: Object;
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
 
 	constructor(private data: DataService) { }
 
   	ngOnInit() {
   		this.data.getUsers().subscribe(
   			data => this.users$ = data
-		)
+		  )
   	}
 
+ 
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+ 
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
+  }
 }
