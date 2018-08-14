d61454d18eff683afce4c257341cc2838b88ae27
mahbubul <rabbi.sus@gmail.com>
2018-08-14 11:42:43 +0600

    test

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,31 +6,31 @@ import { Observable } from 'rxjs';
 const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
 
-	users$: Object;
-  public uploader:FileUploader = new FileUploader({url: URL});
-  public hasBaseDropZoneOver:boolean = false;
-  public hasAnotherDropZoneOver:boolean = false;
+    users$: Object;
+    public uploader: FileUploader = new FileUploader({ url: URL });
+    public hasBaseDropZoneOver: boolean = false;
+    public hasAnotherDropZoneOver: boolean = false;
 
-	constructor(private data: DataService) { }
+    constructor(private data: DataService) { }
 
-  	ngOnInit() {
-  		this.data.getUsers().subscribe(
-  			data => this.users$ = data
-		  )
-  	}
+    ngOnInit() {
+        this.data.getUsers().subscribe(
+            data => this.users$ = data
+        )
+    }
 
- 
-  public fileOverBase(e:any):void {
-    this.hasBaseDropZoneOver = e;
-  }
- 
-  public fileOverAnother(e:any):void {
-    this.hasAnotherDropZoneOver = e;
-  }
+
+    public fileOverBase(e: any): void {
+        this.hasBaseDropZoneOver = e;
+    }
+
+    public fileOverAnother(e: any): void {
+        this.hasAnotherDropZoneOver = e;
+    }
 }
